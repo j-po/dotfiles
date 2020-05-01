482160f374c0609636002521349d7912f150a24f
JPol <3677798+j-po@users.noreply.github.com>
2020-05-01 11:35:44 -0700

    comment out some code that doesn't work, don't use ssh for downloading vim plugins, because why would you need to

--- quick-setup.sh
+++ quick-setup.sh
@@ -1,17 +1,17 @@
-if ! hash brew 2>/dev/null; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-brew install leiningen
-brew install neovim/neovim/neovim
-
-if ! hash pip 2>/dev/null; then
-  python -c "$(curl -fsSL https://bootstrap.pypa.io/get-pip.py)"
-fi
-
-pip install --user neovim
-ln -s .nvimrc ~/.config/nvim/init.vim
-alias vim='nvim'
+#if ! hash brew 2>/dev/null; then
+#  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#fi
+#
+#brew install leiningen
+#brew install neovim/neovim/neovim
+#
+#if ! hash pip 2>/dev/null; then
+#  python -c "$(curl -fsSL https://bootstrap.pypa.io/get-pip.py)"
+#fi
+#
+#pip install --user neovim
+#ln -s .nvimrc ~/.config/nvim/init.vim
+#alias vim='nvim'
 
 echo '{:user {:plugins [[cider/cider-nrepl "0.13.0"]]}}' >> ~/.lein/profiles.clj
 
@@ -19,7 +19,7 @@ tpope () {
   echo "Installing some vim plugins by Tim Pope. Yes, he gets his own function--it's the least I could do."
   pushd ~/.config/nvim/bundle 
   for a in $*; do
-    git clone git@github.com:tpope/vim-$a
+    git clone https://github.com/tpope/vim-$a
   done
   popd
 }
@@ -29,6 +29,6 @@ tpope fireplace sexp-mappings-for-regular-people surround repeat classpath obses
 echo "Installing some other vim plugins."
 pushd ~/.config/nvim/bundle
 for a in ("guns/vim-sexp", "luochen1990/rainbow"); do
-  git clone git@github.com:$a
+  git clone https://github.com/$a
 done
 popd
